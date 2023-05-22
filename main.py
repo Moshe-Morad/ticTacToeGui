518c59eb98eb177a8224fced3dfee448c8f9e263
moshe morad <hvtcob@gmail.com>
2023-05-22 10:51:31 +0300

    tic tac toe game with gui

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#test commit
+import tic_tac_toe
+
+
+def main():
+    """Create the game's board and run its main loop."""
+    game = tic_tac_toe.TicTacToeGame()
+    board = tic_tac_toe.TicTacToeBoard(game)
+    board.mainloop()
+
+
+if __name__ == "__main__":
+    main()
